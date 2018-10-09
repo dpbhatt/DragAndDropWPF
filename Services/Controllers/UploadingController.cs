abaa81eb561e503eb4b8f2dae0d66a4d1e7970a8
DP <dp@sensus.dk>
2018-10-09 17:05:07 +0200

    first commit

---
+++ Services/Controllers/UploadingController.cs
@@ -0,0 +1,65 @@
+﻿using Services.Infrastructure;
+using Services.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace Services.Controllers
+{
+    //https://github.com/filipw/WebApi.Html5.Upload
+
+    public class UploadingController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        public Task<IEnumerable<FileDesc>> Post()
+        {
+            string folderName = "uploads";
+            string PATH = HttpContext.Current.Server.MapPath("~/" + folderName);
+            string rootUrl = Request.RequestUri.AbsoluteUri.Replace(Request.RequestUri.AbsolutePath, String.Empty);
+
+            if (Request.Content.IsMimeMultipartContent())
+            {
+                var streamProvider = new CustomMultipartFormDataStreamProvider(PATH);
+                var task = Request.Content.ReadAsMultipartAsync(streamProvider).ContinueWith<IEnumerable<FileDesc>>(t =>
+                {
+
+                    if (t.IsFaulted || t.IsCanceled)
+                    {
+                        throw new HttpResponseException(HttpStatusCode.InternalServerError);
+                    }
+
+                    var fileInfo = streamProvider.FileData.Select(i =>
+                    {
+                        var info = new FileInfo(i.LocalFileName);
+                        return new FileDesc(info.Name, rootUrl + "/" + folderName + "/" + info.Name, info.Length / 1024);
+                    });
+                    return fileInfo;
+                });
+
+                return task;
+            }
+            else
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotAcceptable, "This request is not properly formatted"));
+            }
+
+        }
+    }
+}
---
+++ Services/Models/FileDesc.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+namespace Services.Models
+{
+    [DataContract]
+    public class FileDesc
+    {
+        [DataMember]
+        public string name { get; set; }
+        [DataMember]
+        public string path { get; set; }
+        [DataMember]
+        public long size { get; set; }
+        public FileDesc(string n, string p, long s)
+        {
+            name = n;
+            path = p;
+            size = s;
+        }
+    }
+}
